5656d541931312f97f3da9df8dc384f5015b16d9
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-12-12 18:58:02 -0500

    Variables y entorno

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,16 @@
+console.log("Hola Mundo"); // resultado de la operación de ese metodo
+
+// variables
+
+let a = 10, b = 30; // dificil de leer
+const c = 10; // valor de memoria no podrá cambiar
+
+let d = 10, // valor de memoria no podrá cambiar
+    x = a + b, // valor de memoria no podrá cambiar
+    e = 10;
+
+console.log(x);
+
+// var: evolucionando antes 
+
+// POLYFILL : codigo que provee el funcionamiento de una nueva caracteristica de javaScript(ES6), en versiones viejas como ES5
