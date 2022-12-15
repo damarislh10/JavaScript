e381d30b9f9cad30d8d825ed42b679a38c008e4e
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-12-14 19:17:38 -0500

    Depuración consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -3,14 +3,26 @@ console.log("Hola Mundo"); // resultado de la operación de ese metodo
 // variables
 
 let a = 10, b = 30; // dificil de leer
-const c = 10; // valor de memoria no podrá cambiar
+const c = 'Hola '; // valor de memoria no podrá cambiar
 
-let d = 10, // valor de memoria no podrá cambiar
+let d = "Spiderman", // valor de memoria no podrá cambiar
     x = a + b, // valor de memoria no podrá cambiar
     e = 10;
 
-console.log(x);
+const SALUDO = c + d;
+console.log(SALUDO);
 
+
+// console.log('%c Mis variables', 'color:pink; font-weight: bold');
+// console.log({a}); // metodo función que se encuentra dentro de un objeto
+// console.log({b});
+// console.log({c});
+
+// console.table({a, b, c, d, x });
 // var: evolucionando antes 
 
 // POLYFILL : codigo que provee el funcionamiento de una nueva caracteristica de javaScript(ES6), en versiones viejas como ES5
+
+d= 'Hola de nuevo';
+
+// breakpoint: cuando la aplicación llegue y se detenga en este punto
