7d397c1c7f243d82d73653d53d5dbc1a632da4bb
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-12-16 18:55:43 -0500

    Alert, props, confirm js

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,15 @@
+// Tres tipos de forma de ingreso del usuario
+
+alert("Hola mundo")
+let nombre = prompt("¿Cual es tu nombre?") // retorna lo que el usuario haya escrito string
+
+console.log("****"+ nombre + '*****'); // ''
+
+const seleccion = confirm('Está seguro de borrar esto?')
+console.log(seleccion);
+
+// son metodos que realmente viene de windows
+
+// node global
+
+// console.log(global); // en node propiedades
