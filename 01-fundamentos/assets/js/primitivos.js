012b344ac9a2c71a776d2d4bcfbb4d32dbd61868
damarislh10 <cortezhernandezdamaris@gmail.com>
2023-02-14 16:54:15 -0500

    primitivos tipos

---
+++ 01-fundamentos/assets/js/primitivos.js
@@ -0,0 +1,26 @@
+let nombre = 'Peter Parker';
+console.log(nombre)
+
+nombre = 'Ben Parker'; // es mas ligero que la comilla doble
+console.log(nombre);
+
+nombre = "Tia May"; // mas util y hacer que el codigo sea mas facil de ver
+nombre = `Tia May`; // mas aplicaciones el delimitador
+
+// operador interno que existe 
+console.log(typeof nombre);
+nombre = 123
+console.log(typeof nombre)
+
+let esMarvel = true;
+
+// numeros
+let edad = 33;
+edad = 33.001
+console.log(typeof edad)
+
+// calmel Case: iniciar la segundo en mayuscula capitalizada
+
+let superPoderDeSpiderMan;
+console.log(superPoderDeSpiderMan)
+let super_poder // no es tan comun en js
